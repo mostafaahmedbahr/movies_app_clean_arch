b85b1e5c6638a7ac8f259b95bfe83f578b514b66
mostafa <mustafa.bahr12323@gmail.com>
2023-10-18 11:05:20 +0300

    3 use cases in domain layer

---
+++ lib/features/movies_feauters/domain/use_cases/fetch_popular_movies_use_case.dart
@@ -0,0 +1,18 @@
+  import 'package:dartz/dartz.dart';
+import 'package:movies_app_clean_arch/features/movies_feauters/domain/entities/movie_entity.dart';
+import 'package:movies_app_clean_arch/features/movies_feauters/domain/repos/base_movie_repo.dart';
+
+import '../../../../core/errors/error_class.dart';
+import '../../../../core/use_cases/use_case_with_no_param.dart';
+
+class FetchPopularMoviesUseCase extends UseCaseWithNoParam<List<MovieEntity>>
+{
+  final BaseMovieRepo baseMovieRepo;
+  FetchPopularMoviesUseCase(this.baseMovieRepo);
+
+  @override
+  Future<Either<Errors, List<MovieEntity>>> call() async {
+    return await baseMovieRepo.fetchPopularMovies();
+
+  }
+}
