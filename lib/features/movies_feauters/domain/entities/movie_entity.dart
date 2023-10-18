cd9d6cd96e35cc058a6f96c1717889fed7805c17
mostafa <mustafa.bahr12323@gmail.com>
2023-10-18 10:45:41 +0300

    MovieEntity make it  Equatable

--- lib/features/movies_feauters/domain/entities/movie_entity.dart
+++ lib/features/movies_feauters/domain/entities/movie_entity.dart
@@ -1,4 +1,54 @@
-class MovieEntity {
+// class MovieEntity {
+//   final int movieEntityId;
+//   final String movieEntityBackdropPath;
+//   final String movieEntityOriginalTitle;
+//   final String movieEntityOverview;
+//   final num movieEntityVoteAverage;
+//   final num movieEntityVoteCount;
+//   final List<int> movieEntityGenreIds;
+//
+//
+//   // نحط const عشان نمنع التكرار ال object عشان الاداء وال memory
+//
+//   // أو استخدم
+//   const MovieEntity(
+//       {required this.movieEntityId,
+//       required this.movieEntityBackdropPath,
+//       required this.movieEntityOriginalTitle,
+//       required this.movieEntityGenreIds,
+//       required this.movieEntityOverview,
+//       required this.movieEntityVoteAverage,
+//       required this.movieEntityVoteCount});
+//
+//   @override
+//   bool operator ==(Object other) =>
+//       identical(this, other) ||
+//       other is MovieEntity &&
+//           runtimeType == other.runtimeType &&
+//           movieEntityId == other.movieEntityId &&
+//           movieEntityBackdropPath == other.movieEntityBackdropPath &&
+//           movieEntityOriginalTitle == other.movieEntityOriginalTitle &&
+//           movieEntityOverview == other.movieEntityOverview &&
+//           movieEntityVoteAverage == other.movieEntityVoteAverage &&
+//           movieEntityVoteCount == other.movieEntityVoteCount &&
+//           movieEntityGenreIds == other.movieEntityGenreIds;
+//
+//   @override
+//   int get hashCode =>
+//       movieEntityId.hashCode ^
+//       movieEntityBackdropPath.hashCode ^
+//       movieEntityOriginalTitle.hashCode ^
+//       movieEntityOverview.hashCode ^
+//       movieEntityVoteAverage.hashCode ^
+//       movieEntityVoteCount.hashCode ^
+//       movieEntityGenreIds.hashCode;
+// }
+//
+
+
+import 'package:equatable/equatable.dart';
+
+class MovieEntity extends Equatable {
   final int movieEntityId;
   final String movieEntityBackdropPath;
   final String movieEntityOriginalTitle;
@@ -7,12 +57,30 @@ class MovieEntity {
   final num movieEntityVoteCount;
   final List<int> movieEntityGenreIds;
 
-  MovieEntity(
-      {required this.movieEntityId,
-      required this.movieEntityBackdropPath,
-      required this.movieEntityOriginalTitle,
-      required this.movieEntityGenreIds,
-      required this.movieEntityOverview,
-      required this.movieEntityVoteAverage,
-      required this.movieEntityVoteCount});
+
+  // نحط const عشان نمنع التكرار ال object عشان الاداء وال memory
+
+  // أو استخدمEquatable
+  const MovieEntity({required this.movieEntityId,
+    required this.movieEntityBackdropPath,
+    required this.movieEntityOriginalTitle,
+    required this.movieEntityGenreIds,
+    required this.movieEntityOverview,
+    required this.movieEntityVoteAverage,
+    required this.movieEntityVoteCount});
+
+  @override
+  @override
+  List<Object> get props =>
+      [
+        movieEntityId,
+        movieEntityBackdropPath,
+        movieEntityOriginalTitle,
+        movieEntityOverview,
+        movieEntityVoteAverage,
+        movieEntityVoteCount,
+        movieEntityGenreIds,
+      ];
+
+
 }
