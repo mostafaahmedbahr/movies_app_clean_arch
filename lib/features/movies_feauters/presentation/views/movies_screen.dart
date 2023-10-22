aad281c13f22fed946c213591c173310b2d91c9f
mostafa <mustafa.bahr12323@gmail.com>
2023-10-22 11:35:55 +0300

    TopRatedCustomWidget

--- lib/features/movies_feauters/presentation/views/movies_screen.dart
+++ lib/features/movies_feauters/presentation/views/movies_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movies_app_clean_arch/features/movies_feauters/presentation/views/widgets/now_playing_widget.dart';
 import 'package:movies_app_clean_arch/features/movies_feauters/presentation/views/widgets/popular_widget.dart';
+import 'package:movies_app_clean_arch/features/movies_feauters/presentation/views/widgets/top_rated_widget.dart';
 import 'package:shimmer/shimmer.dart';
 
 import '../../../../core/utils/app_constance.dart';
@@ -95,53 +96,7 @@ class MainMoviesScreen extends StatelessWidget {
                 ],
               ),
             ),
-            FadeIn(
-              duration: const Duration(milliseconds: 500),
-              child: SizedBox(
-                height: 170.0,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  itemCount: moviesList.length,
-                  itemBuilder: (context, index) {
-                    final movie = moviesList[index];
-                    return Container(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: InkWell(
-                        onTap: () {
-                          /// TODO : NAVIGATE TO  MOVIE DETAILS
-                        },
-                        child: ClipRRect(
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(8.0)),
-                          child: CachedNetworkImage(
-                            width: 120.0,
-                            fit: BoxFit.cover,
-                            imageUrl:
-                                "${AppConstance.imagesUrlPath}${movie.movieEntityBackdropPath}",
-                            placeholder: (context, url) => Shimmer.fromColors(
-                              baseColor: Colors.grey[850]!,
-                              highlightColor: Colors.grey[800]!,
-                              child: Container(
-                                height: 170.0,
-                                width: 120.0,
-                                decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                              ),
-                            ),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+            const TopRatedCustomWidget(),
             const SizedBox(height: 50.0),
           ],
         ),
