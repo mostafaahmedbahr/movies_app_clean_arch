5d6c6314c81b4c39472bde8a5c9198ce35bcd3a5
mostafa <mustafa.bahr12323@gmail.com>
2023-10-23 13:53:45 +0300

    RecommendationEntity

---
+++ lib/features/movie_details_feauters/domain/entities/recommendation_entity.dart
@@ -0,0 +1,9 @@
+class RecommendationEntity{
+
+  final String image;
+  final String title;
+  final int id ;
+
+  RecommendationEntity({required this.image, required this.title, required this.id});
+
+}
