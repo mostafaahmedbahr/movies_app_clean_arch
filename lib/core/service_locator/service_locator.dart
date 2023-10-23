6833c4ab76c7b6d1fa42652fe231f9375f761975
mostafa <mustafa.bahr12323@gmail.com>
2023-10-23 13:51:07 +0300

    ServiceLocator add 2

--- lib/core/service_locator/service_locator.dart
+++ lib/core/service_locator/service_locator.dart
@@ -1,4 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:movies_app_clean_arch/features/movie_details_feauters/data/data_repos/movies_details_data_repos.dart';
+import 'package:movies_app_clean_arch/features/movie_details_feauters/data/data_sources/movie_details_remote_data_source.dart';
+import 'package:movies_app_clean_arch/features/movie_details_feauters/domain/repos/get_movie_details_repos.dart';
 import 'package:movies_app_clean_arch/features/movie_details_feauters/domain/use_cases/fetch_movie_details_use_case.dart';
 import 'package:movies_app_clean_arch/features/movie_details_feauters/domain/use_cases/fetch_related_movies_use_case.dart';
 import 'package:movies_app_clean_arch/features/movies_feauters/domain/use_cases/fetch_playing_now_movie_use_case.dart';
@@ -36,8 +39,10 @@ class ServiceLocator{
 
     // repo
     getIt.registerLazySingleton<BaseMovieRepo>(() => MovieRepoImplementation( baseMovieRemoteDataSource: getIt()));
+    getIt.registerLazySingleton<BaseGetMovieDetailsRepos>(() => MoviesDetailsDataReposImplementation( baseMovieDetailsRemoteDataSource: getIt()));
 
     // data source
     getIt.registerLazySingleton<BaseMovieRemoteDataSource>(() => MovieRemoteDataSourceImplementation());
+    getIt.registerLazySingleton<BaseMovieDetailsRemoteDataSource>(() => MovieDetailsRemoteDataSourceImplementation());
   }
 }
