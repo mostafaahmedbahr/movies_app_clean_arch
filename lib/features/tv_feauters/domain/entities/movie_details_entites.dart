3d7e138025d69c3e49a655ac88c67bbbea083210
mostafa <mustafa.bahr12323@gmail.com>
2023-10-23 10:54:31 +0300

    MovieDetailsEntities part 2

--- lib/features/tv_feauters/domain/entities/movie_details_entites.dart
+++ lib/features/tv_feauters/domain/entities/movie_details_entites.dart
@@ -8,9 +8,21 @@ class MovieDetailsEntities {
   final num voteCount;
   final num voteAverage;
   final num runTime;
+  final List<Genre> genres;
 
 
-  MovieDetailsEntities(this.runTime, {required this.id, required this.backdropPath, required this.title, required this.posterPath, required this.overview, required this.releaseDate, required this.voteCount, required this.voteAverage, });
+  MovieDetailsEntities(this.runTime, this.genres, {required this.id, required this.backdropPath, required this.title, required this.posterPath, required this.overview, required this.releaseDate, required this.voteCount, required this.voteAverage, });
+
+
+}
+
+
+class Genre
+{
+  final String name;
+  final int id;
+
+  Genre({required this.name, required this.id});
 
 
 }
